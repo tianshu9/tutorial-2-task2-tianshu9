ea2b8645a48a931030fd08c4c702893cf626bed7
tianshu9 <135168073+tianshu9@users.noreply.github.com>
2023-11-16 08:37:24 -0500

    Update callbackFunctions.js

--- callbackFunctions.js
+++ callbackFunctions.js
@@ -1,4 +1,3 @@
-
 let triangleFrame = function(letter)
 {
     let framedLetter = '';
@@ -6,15 +5,21 @@ let triangleFrame = function(letter)
     {
         let line='';
         // write a loop to start the line with 3 - i spaces
-       
+        for (let j = 0; j < 3 - i; j++)
+        {
+            line += '';
+        }
 
         // The third line will contain the framed letter so we need to set the right condition
-        if (/*set the condition to specify the third line */) 
+        if (/*set the condition to specify the third line */ i==2) 
             line += `* ${letter} *`;
         else
         {
             // the rest of the lines of the triangle would contain all stars
             // create a loop to add 2i + 1 stars to the line
+            for (let k = 0; k < 2*i + 1; k++) {
+                line += '*';
+            }
         }
 
         framedLetter += line + '\n';
@@ -30,7 +35,9 @@ let rectangleFrame = function(letter)
     line = "";
 
     //write a loop the add five stars to the line
-   
+    for (let i = 0; i < 5; i++) {
+        line += '*';
+    }
 
     //set the framed letter to be of 3 lines (one line of star, 
     //one line * Letter * and the last line of all stars)
@@ -39,16 +46,21 @@ let rectangleFrame = function(letter)
     return framedLetter;
 }
 
-
 // Set the function definition for the generalFramer so that it would receive two arguments
 //      - the first argument is a callback function that creates the desired frame
 //      - the second argument is the letter to be framed.
-// The retun should be the string of the letter framed by the given framer.
-let generalFramer;
+// The return should be the string of the letter framed by the given framer.
+let generalFramer = function(frameFunction, letter) {
+    return frameFunction(letter);
+}
 
-console.log(generalFramer(/*set the argument to frame the letter A in a triangle*/));
-console.log(generalFramer(/*set the argument to frame the letter B in a triangle*/));
-console.log(generalFramer(/*set the argument to frame the letter C in a triangle*/));
+console.log(generalFramer(triangleFrame, 'A'/*set the argument to frame the letter A in a triangle*/));
+console.log(generalFramer(triangleFrame, 'B'/*set the argument to frame the letter B in a triangle*/));
+console.log(generalFramer(triangleFrame, 'C'/*set the argument to frame the letter C in a triangle*/));
+
+console.log(generalFramer(rectangleFrame, 'A'/*set the argument to frame the letter A in a rectangle*/));
+console.log(generalFramer(rectangleFrame, 'B'/*set the argument to frame the letter B in a rectangle*/));
+console.log(generalFramer(rectangleFrame, 'C'/*set the argument to frame the letter C in a rectangle*/));
 
 console.log(generalFramer(/*set the argument to frame the letter A in a rectangle*/));
 console.log(generalFramer(/*set the argument to frame the letter B in a rectangle*/));
